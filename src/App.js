a628b65febeecb3eaf0ad926c09586f2cd1eed43
Manash Chakrobortty <manash.pstu@gmail.com>
2017-12-30 12:34:55 +0600

    Get ready project with common components

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+import Header from './components/common/Header';
+
+const App = () => (
+  <View>
+    <Header headerText={'Auth'}/>
+  </View>
+);
+export default App;
